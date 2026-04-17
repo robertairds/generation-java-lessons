514ce66a6cd2b958e3f80b4baad5fde8fbd59d1b
Roberta Rodrigues <roberta673@gmail.com>
2026-04-17 10:20:31 -0300

    first commit

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { //método principal que executa o código
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,25 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Variáveis
+		int age = 28;
+		String name = "Roberta";
+		float degree = 20.5f;
+		boolean answer;        // true/false
+		
+		//Constantes
+		final int QUANTITY = 50;
+		final String CLASS = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.println(name);
+		System.out.println(QUANTITY);
+		System.out.println(degree);
+
+	}
+
+}
